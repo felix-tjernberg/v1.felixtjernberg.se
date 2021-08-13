00415a94b3a7c21f13ddfe1c2219771d21055a36
FilleFLEX <felix.tjernberg@hotmail.se>
2021-08-13 15:38:51 +0200

    Refactor webpack configuration
    In the initial implementation of esbuild-loader the user had to
    uncomment a few lines when building for production to get the smallest
    output
    
    This was solved by choosing between two different configuration objects
    at build time decided by process.env.NODE_ENV
    
    The production configuration is using the babel-loader and minifying
    plugins to get the smallest possible output and the development
    configuration is using only esbuild-loader to cut down on build time

--- webpack.config.js
+++ webpack.config.js
@@ -2,95 +2,152 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const { ESBuildMinifyPlugin } = require('esbuild-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const ThreeMinifierPlugin = require('@yushijinhun/three-minifier-webpack')
 const path = require('path')
 
-let devtool = process.env.NODE_ENV ? false : 'source-map'
-let mode = process.env.NODE_ENV || 'development'
-let target = process.env.NODE_ENV ? 'browserslist' : 'web'
-
-const threeMinifier = new ThreeMinifierPlugin()
-
-module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    hot: true
-  },
-  devtool: devtool,
-  entry: {
-    index: path.join(__dirname, 'source', 'index.js')
-  },
-  mode: mode,
-  module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: 'asset'
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-      // {
-      //   loader: 'html-loader',
-      //   test: /\.html$/i
-      // },
-      // {
-      //   exclude: /node_modules/,
-      //   test: /\.js$/i,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       cacheDirectory: true,
-      //       presets: ['@babel/preset-env']
-      //     }
-      //   }
-      // }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      // '...', // Uncomment this and comment ESBuildMinifyPlugin to save 3% of build weight
-      new ESBuildMinifyPlugin({
-        target: 'es2015'
-      }),
-      new CssMinimizerPlugin({
-        minify: CssMinimizerPlugin.cleanCssMinify,
-        minimizerOptions: { level: 2 }
-      })
-    ],
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          chunks: 'all',
-          name: 'vendors',
-          test: /[\\/]node_modules[\\/]/
+if (process.env.NODE_ENV) {
+  // Production configuration
+  const ThreeMinifierPlugin = require('@yushijinhun/three-minifier-webpack')
+  const threeMinifier = new ThreeMinifierPlugin()
+  module.exports = {
+    devServer: {
+      contentBase: path.join(__dirname, 'build')
+    },
+    devtool: false,
+    entry: {
+      index: path.join(__dirname, 'source', 'index.js')
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+          type: 'asset'
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          loader: 'html-loader',
+          test: /\.html$/i
+        },
+        {
+          exclude: /node_modules/,
+          test: /\.js$/i,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin({
+          minify: CssMinimizerPlugin.cleanCssMinify,
+          minimizerOptions: { level: 2 }
+        })
+      ],
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            name: 'vendors',
+            test: /[\\/]node_modules[\\/]/
+          }
         }
       }
-    }
-  },
-  output: {
-    assetModuleFilename: 'image/[hash][ext][query]',
-    clean: true,
-    filename: './javascript/[name].[contenthash].js',
-    path: path.join(__dirname, 'build')
-  },
-  plugins: [
-    // threeMinifier, // Uncomment this and plugins key in resolve to save 200kb
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({ template: './source/index.html' })
-  ],
-  resolve: {
-    alias: {
-      image: './image',
-      javascript: './javascript',
-      stylesheet: './stylesheet',
-      svg: './svg'
-    }
-    // plugins: [threeMinifier.resolver]
-  },
-  stats: 'errors-warnings',
-  target: target
+    },
+    output: {
+      assetModuleFilename: 'image/[hash][ext][query]',
+      clean: true,
+      filename: './javascript/[name].[contenthash].js',
+      path: path.join(__dirname, 'build')
+    },
+    plugins: [
+      threeMinifier,
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({ template: './source/index.html' })
+    ],
+    resolve: {
+      alias: {
+        image: './image',
+        javascript: './javascript',
+        stylesheet: './stylesheet',
+        svg: './svg'
+      },
+      plugins: [threeMinifier.resolver]
+    },
+    target: 'browserslist'
+  }
+} else {
+  // Development configuration
+  module.exports = {
+    devServer: {
+      contentBase: path.join(__dirname, 'build'),
+      hot: true
+    },
+    devtool: 'source-map',
+    entry: {
+      index: path.join(__dirname, 'source', 'index.js')
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+          type: 'asset'
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new ESBuildMinifyPlugin({
+          css: true,
+          target: 'es2015'
+        })
+      ],
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            name: 'vendors',
+            test: /[\\/]node_modules[\\/]/
+          }
+        }
+      }
+    },
+    output: {
+      assetModuleFilename: 'image/[hash][ext][query]',
+      clean: true,
+      filename: './javascript/[name].[contenthash].js',
+      path: path.join(__dirname, 'build')
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({ template: './source/index.html' })
+    ],
+    resolve: {
+      alias: {
+        image: './image',
+        javascript: './javascript',
+        stylesheet: './stylesheet',
+        svg: './svg'
+      }
+    },
+    stats: 'errors-warnings',
+    target: 'web'
+  }
 }
