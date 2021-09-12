bb7bdfba69c6083693938c1dfbfa1dc0a27baa71
FilleFLEX <felix.tjernberg@hotmail.se>
2021-09-12 07:15:55 +0200

    Add initial content

--- source/index.js
+++ source/index.js
@@ -1,9 +1,10 @@
 import 'Stylesheet/reset.css'
 import 'Stylesheet/test.css'
+import 'Stylesheet/global.css'
 
-import(
-  /* webpackChunkName: "landing-scene" */ 'Javascript/landing-scene/landing-scene.js'
-)
+// import(
+//   /* webpackChunkName: "landing-scene" */ 'Javascript/landing-scene/landing-scene.js'
+// )
 // ).then((module) => {
 //   const renderFrame = module.default
 //   renderFrame()
