4fbcaf87b7821f1817e4bb5a96d63bf4191592cd
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-01-20 10:08:54 +0100

    Refactor toggleAllowProcessIntesive

--- source/javascript/toggleAllowProcessIntensive.js
+++ source/javascript/toggleAllowProcessIntensive.js
@@ -1,8 +1,6 @@
 export default function toggleAllowProcessIntensive() {
   const glassElements = document.querySelectorAll('.glass-background')
   glassElements.forEach((element) => {
-    return element.dataset.allowProcessIntensive
-      ? element.removeAttribute('data-allow-process-intensive')
-      : element.setAttribute('data-allow-process-intensive', 'true')
+    element.toggleAttribute('data-allow-process-intensive')
   })
 }
