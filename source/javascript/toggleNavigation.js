bf4588b14c7fb3a273396b299d38d08ed3d70675
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-01-22 12:17:49 +0100

    Remove console log

--- source/javascript/toggleNavigation.js
+++ source/javascript/toggleNavigation.js
@@ -1,5 +1,4 @@
 export default function toggleNavigation(target) {
-  console.log(target)
   target.previousSibling.toggleAttribute('data-show-navigation')
   target.textContent =
     target.textContent == 'Hide navigation'
